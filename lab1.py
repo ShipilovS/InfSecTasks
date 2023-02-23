46965671aece2954a60b251f333afa286d98e01e
Shipilov Stefan <stefan.shipilov@mail.ru>
2023-02-24 00:11:14 +0300

    normal fix

--- lab1.py
+++ lab1.py
@@ -51,22 +51,23 @@ def ROL(x, n, bits=32):
     return ROR(x, bits - n, bits)
 
 def vlevo(x, t):
-    return (x << t) | (x >> (32 - t))
+    return ctypes.c_uint32((x << t) | (x >> (32 - t))).value
 
 def vpravo32(x, t, bits=32):
-    return (x >> t) | (x << (32 - t))
+    return ctypes.c_uint32((x >> t) | (x << (32 - t))).value
 
 def vpravo64(x, t):
     return ctypes.c_uint64((x >> t) | (x << (64 - t))).value
 
 def F(L : int, K : int):
     f1 = vlevo(L, 9)
-    f2 = vpravo32(K, 11) # | L
+    f2 = ctypes.c_uint32(vpravo32(K, 11) | L).value
+    print(f"f1 = {f1}")
+    print(f"f2 = {f2}")
     return f1 ^ f2
 
 def Ki(i):
     binary_value = vpravo64(convert_from_hex_to_decimal(KEY), 8*i) # с 0b + 32 бита или [2:34]
-    print(binary_value)
     return binary_value
 # ==
 
@@ -86,12 +87,12 @@ def encoding(msg):
         print(f"IN lev_b \t= {lev_b}({hex(lev_b)})")
         print(f"IN prav_b \t= {prav_b}({hex(prav_b)})\n")
 
-        if (i < ROUNDS - 1):
-            # print('YES')
+        if (i != ROUNDS - 1):
+            print('YES')
             lev_b = prav_i 
             prav_b = lev_i
         else:
-            # print('NO')
+            print('NO')
             lev_b = lev_i
             prav_b = prav_i
         print(f"OUT lev_b \t= {lev_b}({hex(lev_b)})")
@@ -105,14 +106,13 @@ def encoding(msg):
 def decoding(e_msg):
     lev_b   = ctypes.c_uint32((convert_from_hex_to_decimal(e_msg) >> 32) & convert_from_hex_to_decimal(F32)).value
     prav_b  = ctypes.c_uint32(convert_from_hex_to_decimal(e_msg) & convert_from_hex_to_decimal(F32)).value
-    for i in range(ROUNDS-1, -1, -1):
+    for i in range(ROUNDS - 1, -1, -1):
         K_i = Ki(i)
         lev_i = lev_b
+        print(f"FKI = {K_i}")
         prav_i = prav_b ^ F(lev_b, K_i)
-        print(f"IN lev_i \t= {lev_b}({hex(lev_b)})")
-        print(f"IN prav_i \t= {prav_b}({hex(prav_b)})\n")
 
-        if (i > 0):
+        if (i != 0):
             # print('YES')
             lev_b = prav_i 
             prav_b = lev_i
@@ -120,8 +120,6 @@ def decoding(e_msg):
             # print('NO')
             lev_b = lev_i
             prav_b = prav_i
-        print(f"OUT lev_i \t= {lev_b}")
-        print(f"OUT prav_i \t= {prav_b}\n")
     shifroblok = lev_b
     shifroblok = ctypes.c_uint32((shifroblok << 32) | (prav_b & convert_from_hex_to_decimal(F32))).value
     return shifroblok
