acbca14afe96e6af7d54b9b1a6dbbe6a3b95fc71
Shipilov Stefan <stefan.shipilov@mail.ru>
2023-03-31 23:25:58 +0300

    task3

--- lab3.py
+++ lab3.py
@@ -1,12 +1,13 @@
 # https://ru.frwiki.wiki/wiki/G%C3%A9n%C3%A9rateur_congruentiel_lin%C3%A9aire
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-m = 25
+m = 509
 c = 16
-p = 256
+p = 24551
 
-N = 33
+N = 10001
 
 def X(x, m, c, p):
     print(f"{x}*{m}({x*m}) + {c})({x*m + c}) % m === {(x*m + c) % p}")
@@ -17,12 +18,19 @@ start_seed = 125
 R = [start_seed / p]
 seeds = [start_seed]
 for i in range(N):
+    if len(np.unique(seeds)) != len(seeds):
+        print(start_seed)
+        break
     start_seed = X(start_seed, m, c, p)
-    print(start_seed)
     R.append(start_seed / p)
     seeds.append(start_seed)
 
-print(seeds)
-print(R)
+
+# print(seeds)
+# print(R)
+print(f"\n\n")
 print(len(seeds))
 print(len(np.unique(seeds)))
+fig, ax = plt.subplots()
+plt.plot(R)
+plt.show()
