21dc5614a45968b3575a6d9445a5bec459035d91
Stefan Shipilov <stefan.shipilov@mail.ru>
2023-04-01 13:07:48 +0300

    task3 в ipynb

--- lab3.py
+++ lab3.py
@@ -5,32 +5,40 @@ import matplotlib.pyplot as plt
 
 m = 509
 c = 16
-p = 24551
+p = 229
+u_0 = 12
 
 N = 10001
 
 def X(x, m, c, p):
-    print(f"{x}*{m}({x*m}) + {c})({x*m + c}) % m === {(x*m + c) % p}")
+    # print(f"{x}*{m}({x*m}) + {c})({x*m + c}) % m === {(x*m + c) % p}")
     return (x*m + c) % p
 
 
-start_seed = 125
-R = [start_seed / p]
-seeds = [start_seed]
-for i in range(N):
-    if len(np.unique(seeds)) != len(seeds):
-        print(start_seed)
-        break
-    start_seed = X(start_seed, m, c, p)
-    R.append(start_seed / p)
-    seeds.append(start_seed)
+def lehmer_generator(u_0, m, c, p):
+    R = [u_0 / p]
+    u_i = [u_0]
+    while (len(np.unique(u_i)) < 10000) and (len(np.unique(u_i)) == len(u_i)): 
+        temp = X(u_i[-1], m, c, p)
+        R.append(temp / p)
+        u_i.append(temp)
+    return u_i, R
+
+def length_test(m, c, p):
+    for u_0 in range(10):
+        u_values, r_values = lehmer_generator(u_0, m, c, p)
+        print(f"При u_0 = {u_0} len(u_values) = {len(u_values)}")
 
 
 # print(seeds)
 # print(R)
 print(f"\n\n")
-print(len(seeds))
-print(len(np.unique(seeds)))
+# u_values, r_values = lehmer_generator(u_0, m, c, p)
+# print(f"len(u_values) = {len(u_values)}")
+
+length_test(m, c, p)
+
 fig, ax = plt.subplots()
-plt.plot(R)
-plt.show()
+# plt.plot(r_values)
+# plt.show()
+
